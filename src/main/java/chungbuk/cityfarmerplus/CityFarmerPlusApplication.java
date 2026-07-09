397176d02f410d524b6ac5e555586ffb7d92afa8
ParkChanWoo0321 <op9563_@naver.com>
2026-07-09 21:17:41 +0900

    프로젝트 생성

---
+++ src/main/java/chungbuk/cityfarmerplus/CityFarmerPlusApplication.java
@@ -0,0 +1,13 @@
+package chungbuk.cityfarmerplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityFarmerPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityFarmerPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chungbuk/cityfarmerplus/CityFarmerPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package chungbuk.cityfarmerplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityFarmerPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
