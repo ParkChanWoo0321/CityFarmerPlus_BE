cf389d99b23532760bf2c276a0006d79b0ab8d54
ParkChanWoo0321 <op9563_@naver.com>
2026-07-26 22:58:25 +0900

    MySQL 및 JWT 로컬 실행 환경 구성

--- src/test/java/chungbuk/cityfarmerplus/CityFarmerPlusApplicationTests.java
+++ src/test/java/chungbuk/cityfarmerplus/CityFarmerPlusApplicationTests.java
@@ -1,9 +1,12 @@
 package chungbuk.cityfarmerplus;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfEnvironmentVariable(named = "DB_PASSWORD", matches = ".+")
+@EnabledIfEnvironmentVariable(named = "JWT_SECRET", matches = ".{32,}")
 class CityFarmerPlusApplicationTests {
 
     @Test
